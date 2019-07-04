318cb161e8c80557328c9b339126b4a5d7d117b4
MaaniBeigy <manibeygi@gmail.com>
2019-07-04 21:53:05 +0430

    :tada: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// rescale
+// Generic feature scaling methods
+// Copyright: 2018, Maani Beigy <manibeygi@gmail.com>, 
+// License: MIT/Apache-2.0
+//! # rescale
+//!
+//! `rescale` is a library for generic feature scaling methods.
+//! 
+fn main() {
+    println!("hello rescale!")
+}
